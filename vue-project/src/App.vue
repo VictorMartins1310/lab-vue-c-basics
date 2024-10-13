fa4252d80c8e7b77d18f4f182a81b4287f5a764f
VictorMartins <Victor.Martins.1310@hotmail.com>
2024-10-14 00:48:17 +0200

    done

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,29 +1,104 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 
-import  NavBar from "./components/NavBar.vue";
+import NavBar from "./components/NavBar.vue";
 import Footer from './components/Footer.vue';
+import { reactive, ref } from 'vue';
+
+let showElement = ref(true);
+let message = ref("Hide Element");
+
+function showHideElement(){
+  showElement = !showElement;
+  if (showElement){
+    message.value = "Hide Element";
+  }
+  else{
+    message.value = "Show Element";
+  }
+}
+
+let arrayData = [
+  {
+    title: "Film",
+    description: "somthing about Movie",
+    content: "Something interesting"
+  },
+  {
+    title: "Serie",
+    description: "A nice history",
+    content: "Lot of Seasons"
+  },
+  {
+    title: "Anime",
+    description: "Dragon Ball is the best",
+    content: "Surprise"
+  },
+  {
+    title: "Book",
+    description: "Something to read",
+    content: "Lot of Pages to read"
+  }
+];
+
+let bgColor = ref("grey");
+
+function changeBGColor() {
+  console.log(bgColor.value);
+  bgColor.value = bgColor.value;
+}
+
+let number1 = ref(3), number2 = ref(4);
+
+function sumTwoNumbers(number1, number2) {
+  console.log(number1+number2);
+  return number1 + number2;
+}
+
+let result = ref(sumTwoNumbers(number1.value, number2.value));
 
 </script>
 
 <template>
   <NavBar></NavBar>
-  <!-- <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="container" v-bind:style="{ 'background-color': bgColor }">
+    <div class="sum-operation small-container">
+      <input type="number" v-model="number1">
+      <input type="number" v-model="number2">
+      <button v-on:click="result = sumTwoNumbers(number1, number2)">Sum Numbers</button>
+      <p>Result: {{ result }}</p>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main> -->
+    <div class="small-container">
+      <p v-if="showElement">Element to Hide</p>
+      <button v-on:click="showHideElement()">{{ message }}</button>
+    </div>
+    <div class="small-container">
+      <h1>List of Elements</h1>
+      <p v-for="item in arrayData">{{ item.title }} - {{ item.description }} - {{ item.content }} </p>
+    </div>
+    <div class="small-container">
+      <p>Background Color </p><input type="text" placeholder="Background Color" v-model="bgColor" />
+    </div>
+  </div>
   <Footer></Footer>
 </template>
 
 <style scoped>
+.container {
+  background: #334;
+}
+
+.small-container {
+  width: 600px;
+  background-color: yellowgreen;
+  padding: 20px;
+  margin: 20px;
+}
+
+.sum-operation {
+  height: 200px;
+  background-color: yellow;
+}
+
 header {
   line-height: 1.5;
 }
--- vue-project/src/components/NavBar.vue
+++ vue-project/src/components/NavBar.vue
@@ -2,8 +2,6 @@
 </script>
 
 <template>
-
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
